af311a5d3edf9c2968695164ea599aa203253181
Utkarsh Mishra <utkarshsrmcem111@gmail.com>
2021-11-22 10:03:29 +0530

    bug fixing

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -22,13 +22,13 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     @cart=current_cart
-    product = Product.find(params[:products_id])
+    product = Product.find(params[:products_id].to_i)
     # @line_item = LineItem.new(line_item_params)
     @line_item = @cart.add_product(product.id)
-
+    byebug
     respond_to do |format|
 
-      if @line_item.save
+      if @line_item.save!
         format.html { redirect_to @line_item.cart, notice: "Line item was successfully created." }
         format.json { render :show, status: :created, location: @line_item }
         puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
